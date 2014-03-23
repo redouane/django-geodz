947109de7deae58eaed9fe92b98f3fab2baf4bb2
Redouane Zait <redouanezait@gmail.com>
2014-03-23 20:17:31 +0100

    added gmaps link on list_display, municipality count, better admin integration, and more

--- geodz/admin.py
+++ geodz/admin.py
@@ -1,12 +1,36 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.templatetags.static import static
+
 from models import Municipality, Province
 
 
-class MunicipalityAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+class GeodzModelAdmin(admin.ModelAdmin):
 
-class ProvinceAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name',)
+    def google_maps(self, obj):
+
+        html = u'<a target="_blank" href="{0}"><img src="{1}" width="16" height="16" alt="" /></a>'
+
+        return format_html(html,
+            obj.get_gmaps_query_url(),
+            static('img/gmaps.png')
+        )
+
+    google_maps.allow_tags = True
+
+
+class MunicipalityAdmin(GeodzModelAdmin):
+    list_display = ('name', 'province', 'google_maps')
+    list_filter = ('province',)
+    search_fields = ('name',)
+
+class ProvinceAdmin(GeodzModelAdmin):
+    list_display = ('code', 'name', 'municipality_count', 'google_maps')
+    list_display_links = ('name',)
+    search_fields = ('name', 'code',)
+
+    def municipality_count(self, obj):
+        return obj.municipalities.count()
 
 admin.site.register(Municipality, MunicipalityAdmin)
 admin.site.register(Province, ProvinceAdmin)
--- geodz/models.py
+++ geodz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Province(models.Model):
     ### wilaya ###
@@ -6,13 +7,19 @@ class Province(models.Model):
     class Meta:
         verbose_name = 'Province'
         verbose_name_plural = 'Provinces'
+        ordering = ['id']
 
-    name = models.CharField('Nom', max_length=255)
+    name = models.CharField('Name', max_length=255)
     code = models.CharField('Code', max_length=2)
 
     #geo coords
     longitude = models.FloatField(blank=True, null=True)
     latitude = models.FloatField(blank=True, null=True)
+
+
+    def get_gmaps_query_url(self):
+        url = getattr(settings, 'GEODZ_GMAPS_QUERY_URL', 'http://maps.google.com/?q=')
+        return '%s%s' % (url, self.name)
 
     def __unicode__(self):
         return self.name
@@ -21,10 +28,11 @@ class Municipality(models.Model):
     ### commune ###
 
     class Meta:
-        verbose_name = 'Commune'
-        verbose_name_plural = 'Communes'
+        verbose_name = 'Municipality'
+        verbose_name_plural = 'Municipalities'
+        ordering = ['province']
 
-    name = models.CharField('Nom', max_length=255)
+    name = models.CharField('Name', max_length=255)
 
     #geo coords
     longitude = models.FloatField(blank=True, null=True)
@@ -32,6 +40,10 @@ class Municipality(models.Model):
 
     #fk
     province = models.ForeignKey(Province, related_name='municipalities')
+
+    def get_gmaps_query_url(self):
+        url = getattr(settings, 'GEODZ_GMAPS_QUERY_URL', 'http://maps.google.com/?q=')
+        return '%s%s, %s' % (url, self.name, self.province)
 
     def __unicode__(self):
         return self.name
