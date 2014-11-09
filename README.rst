95d31474ab1fba655f5f5de44a87a5a2ffd35a69
Redouane Zait <redouanezait@gmail.com>
2014-11-09 17:33:13 +0100

    documented installed_apps settings

--- README.rst
+++ README.rst
@@ -35,6 +35,7 @@ Quick start
 
     INSTALLED_APPS = (
         ...
+        'geoposition',
         'geodz',
     )
 
--- geodz/__init__.py
+++ geodz/__init__.py
@@ -3,5 +3,6 @@ from django.conf import settings
 # django 1.7 app conf
 default_app_config = 'geodz.apps.GeodzConfig'
 
+# only works pre 1.7
 if 'geoposition' not in settings.INSTALLED_APPS:
     settings.INSTALLED_APPS += ('geoposition',)
